0bf3a57c832dc61b47b37a113b4e2c2d96e89c61
Vipin <vipin@cbio.mskcc.org>
2015-11-06 12:27:47 -0500

    gffwriter module added to this

--- GFFParser.py
+++ GFFParser.py
@@ -507,3 +507,53 @@ def create_missing_feature_type(p_feat, c_feat):
                                             gene_id = '' ))
     return p_feat, child_n_map 
 
+
+def GFFWriter(gtf_content, out_file="out.gff"):
+    """
+    write the feature information to GFF format
+
+    @args gtf_content: Parsed object from gtf file 
+    @type gtf_content: numpy array
+    """
+
+    outfh = open(out_file, "w") 
+    outfh.write('##gff-version 3\n')
+
+    for ent1 in gtf_content:
+        chr_name = ent1['chr']
+        strand = ent1['strand']
+        start = ent1['start']
+        stop = ent1['stop']
+        source = ent1['source']
+        ID = ent1['name']
+        Name = ent1['gene_info']['Name']
+        Name = ID if not Name else Name 
+
+        outfh.write('%s\t%s\tgene\t%d\t%d\t.\t%s\t.\tID=%s;Name=%s\n' \
+            % (chr_name, source, start, stop, strand, ID, Name))
+
+        for idx, tid in enumerate(ent1['transcripts']):
+            t_start = ent1['exons'][idx][0][0]
+            t_stop = ent1['exons'][idx][-1][-1]
+            t_type = ent1['transcript_type'][idx]
+
+            outfh.write('%s\t%s\t%s\t%d\t%d\t.\t%s\t.\tID=%s;Parent=%s\n' \
+                % (chr_name, source, t_type, t_start, t_stop, strand, tid[0], ID))
+
+            for ex_cod in ent1["utr5_exons"][idx]:
+                outfh.write('%s\t%s\tfive_prime_UTR\t%d\t%d\t.\t%s\t.\tParent=%s\n' \
+                    % (chr_name, source, ex_cod[0], ex_cod[1], strand, tid[0]))
+
+            for ex_cod in ent1['cds_exons'][idx]:
+                outfh.write('%s\t%s\tCDS\t%d\t%d\t.\t%s\t%d\tParent=%s\n' \
+                    % (chr_name, source, ex_cod[0], ex_cod[1], strand, ex_cod[2], tid[0]))
+
+            for ex_cod in ent1["utr3_exons"][idx]:
+                outfh.write('%s\t%s\tthree_prime_UTR\t%d\t%d\t.\t%s\t.\tParent=%s\n' \
+                    % (chr_name, source, ex_cod[0], ex_cod[1], strand, tid[0]))
+
+            for ex_cod in ent1['exons'][idx]:
+                outfh.write('%s\t%s\texon\t%d\t%d\t.\t%s\t.\tParent=%s\n' \
+                    % (chr_name, source, ex_cod[0], ex_cod[1], strand, tid[0]))
+
+    outfh.close()  
